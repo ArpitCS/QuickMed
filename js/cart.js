a0280743d85ab9aefc2365a065b291a7d3c0cf73
Arpit Garg <arpit2005.garg@gmail.com>
2024-08-24 19:30:58 +0530

    Home, Store, Cart, Consult, Tests Created

---
+++ js/cart.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const cartItemsContainer = document.getElementById("cartItems");
+  const totalPriceElement = document.getElementById("totalPrice");
+//   const cartAmountElement = document.getElementById("cart-amount");
+
+  function loadCart() {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    updateCart(cart);
+  }
+
+  window.addToCart = function (productId) {
+    const product = products.find((p) => p.id === productId);
+    if (!product) return;
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart.push(product);
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    updateCartAmount();
+  };
+
+  function updateCart(cart) {
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach((item) => {
+      if (item.image === undefined) {
+        item.image = "../assets/testsIcon.png";
+      }
+      const cartItemElement = document.createElement("div");
+      cartItemElement.className = "cartCard row";
+      cartItemElement.innerHTML = `
+                    <div class="cardTitle col-md-3 justify-content-start">
+                        <img src="${item.image}" alt="${item.name}">
+                        <span>${item.name}</span>
+                    </div>
+                    <span  class="col-md-3">Rs. ${item.price}</span>
+                    <button class="btn btn-danger btn-sm" onclick="removeFromCart(${item.id})">Remove</button>
+            `;
+      cartItemsContainer.appendChild(cartItemElement);
+      total += item.price;
+    });
+
+    totalPriceElement.textContent = `Total: Rs.${total}`;
+  }
+
+  function removeFromCart(productId) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart = cart.filter((p) => p.id !== productId);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    updateCart(cart);
+
+    alert("Item removed from cart");
+    updateCartAmount();
+  }
+
+  window.removeFromCart = removeFromCart;
+
+  loadCart();
+  updateCartAmount();
+});
---
+++ js/index.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const cartAmountElement = document.getElementById("cart-amount");
+  
+    function updateCartAmount() {
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+      let totalAmount = cart.reduce((total, item) => total + item.price, 0);
+      cartAmountElement.textContent = `${totalAmount}`;
+    }
+  
+    const searchBar = document.getElementById("searchInput");
+    const searchButton = document.getElementById("searchButton");
+  
+    searchButton.addEventListener("click", function () {
+      const query = searchBar.value;
+      const currentUrl = window.location.href;
+      window.location.href = currentUrl.includes("html")
+        ? `../html/store.html?search=${query}`
+        : `store.html?search=${query}`;
+    });
+  
+    updateCartAmount();
+  
+    const consultButton = document.getElementById("consultBtn");
+    const consultModalElement = document.querySelector("#consultModalContainer .modal");
+    const consultModal = new bootstrap.Modal(consultModalElement);
+    
+    consultButton.addEventListener("click", function () {
+      consultModal.toggle();
+    });
+
+    const closeButton = document.getElementById('closeBtn');
+
+    closeButton.addEventListener('click', function() {
+        consultModal.toggle();
+    });
+
+  });
