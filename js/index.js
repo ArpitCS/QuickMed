3d3de9a993345a25b18b63c08f615be8fe12b13b
Arpit Garg <arpit2005.garg@gmail.com>
2024-08-29 18:23:25 +0530

    Fixed Search Bar

--- js/index.js
+++ js/index.js
@@ -14,8 +14,8 @@ document.addEventListener("DOMContentLoaded", function () {
       const query = searchBar.value;
       const currentUrl = window.location.href;
       window.location.href = currentUrl.includes("html")
-        ? `QuickMed/html/store.html?search=${query}`
-        : `QuickMed/store.html?search=${query}`;
+        ? `html/store.html?search=${query}`
+        : `store.html?search=${query}`;
     });
   
     updateCartAmount();
